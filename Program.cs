fd879ffff1bb8eb2c0208c4a8b340e8fbec064d7
vijaydwivedi125 <vijaydwivedi125@gmail.com>
2022-04-22 22:25:40 +0530

    removing negative elements from list

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Listproblem
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			List<int> Integers = new List<int> { -1, 1, -2, 2, -3, 3, -4, 4, -5, 5 };
+
+			Integers.RemoveAll(t => t < 0);
+
+			foreach (int i in Integers.ToList())
+			{
+				Console.WriteLine(i);
+			}
+
+			Console.ReadLine();
+
+		}
+
+	}
+}
