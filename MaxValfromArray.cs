a3ee0864d5cf23ad60e0345bc2b0be52f6d116f5
vijaydwivedi125 <vijaydwivedi125@gmail.com>
2022-05-05 21:55:15 +0530

    Min values from array

--- MaxValfromArray.cs
+++ MaxValfromArray.cs
@@ -8,23 +8,23 @@ namespace Coding_Exercise_for_Interview
 {
     class MaxValfromArray
     {
-        public static void Main()
-        {
-            int[] arr = { 15, 18, 1, 9, 45, 87, 89 };
-            int max = arr[0];
+        //public static void Main()
+        //{
+        //    int[] arr = { 15, 18, 1, 9, 45, 87, 89 };
+        //    int max = arr[0];
 
-            for (int i = 0; i < arr.Length;i++)
-            {
-                if (arr[i]>max)
-                {
-                    max = arr[i];
-                }
+        //    for (int i = 0; i < arr.Length;i++)
+        //    {
+        //        if (arr[i]>max)
+        //        {
+        //            max = arr[i];
+        //        }
 
-            }
+        //    }
 
-            Console.WriteLine("Highest Value :" + max);
-            Console.ReadLine();
-        }
+        //    Console.WriteLine("Highest Value :" + max);
+        //    Console.ReadLine();
+        //}
 
     }
 }
