aebf52cfc4ce0565c4266640a1f640cdcbb20c64
vijaydwivedi125 <vijaydwivedi125@gmail.com>
2022-05-05 22:09:25 +0530

    Star pattern Up

--- Star_Pattern_Down.cs
+++ Star_Pattern_Down.cs
@@ -8,18 +8,18 @@ namespace Coding_Exercise_for_Interview
 {
     class Star_Pattern_Down
     {
-        public static void Main()
-        {
-            for(int i=5;i>=1;i--)
-            {
-                for(int j=1;j<=i;j++)
-                {
-                    Console.Write("*");
-                }
+        //public static void Main()
+        //{
+        //    for(int i=5;i>=1;i--)
+        //    {
+        //        for(int j=1;j<=i;j++)
+        //        {
+        //            Console.Write("*");
+        //        }
 
-                Console.WriteLine();
-            }
-            Console.ReadLine();
-        }
+        //        Console.WriteLine();
+        //    }
+        //    Console.ReadLine();
+        //}
     }
 }
--- Star_Pattern_UP.cs
+++ Star_Pattern_UP.cs
@@ -8,5 +8,18 @@ namespace Coding_Exercise_for_Interview
 {
     class Star_Pattern_UP
     {
+        public static void Main()
+        {
+            for (int i = 1; i <=5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
     }
 }
