163e71a26f6a975c8357e4bf0283af20e57b1ebd
vijaydwivedi125 <vijaydwivedi125@gmail.com>
2022-05-05 22:07:45 +0530

    down star pattern

--- MinValfromArray.cs
+++ MinValfromArray.cs
@@ -8,22 +8,22 @@ namespace Coding_Exercise_for_Interview
 {
     class MinValfromArray
     {
-        public static void Main()
-        {
-            int[] arr = { 15, 18, 1, 9, 45, 87, 89 };
-            int min = arr[0];
+        //public static void Main()
+        //{
+        //    int[] arr = { 15, 18, 1, 9, 45, 87, 89 };
+        //    int min = arr[0];
 
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (arr[i] < min)
-                {
-                    min = arr[i];
-                }
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        if (arr[i] < min)
+        //        {
+        //            min = arr[i];
+        //        }
 
-            }
+        //    }
 
-            Console.WriteLine("Minimum Value :" + min);
-            Console.ReadLine();
-        }
+        //    Console.WriteLine("Minimum Value :" + min);
+        //    Console.ReadLine();
+        //}
     }
 }
