966498b47757a7691f9d3dc5ae2db2a753331463
vijaydwivedi125 <vijaydwivedi125@gmail.com>
2022-05-09 21:21:30 +0530

    find missing number from array

---
+++ Find_Missing_Number_from_Array.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Exercise_for_Interview
+{
+    class Find_Missing_Number_from_Array
+    {
+        public static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 6, 7, 8, 9, 10 };
+
+            int missingNumber, Totalsum;
+
+            Totalsum = (arr.Length + 1) * (arr.Length + 2) / 2;
+
+
+            foreach (int item in arr)
+            {
+                Totalsum = Totalsum - item;
+            }
+            missingNumber = Totalsum;
+
+            Console.WriteLine("missing number  : {0}", missingNumber);
+            Console.ReadLine();
+        }
+
+        
+    }
+}
--- Star_Pyramid_Pattern.cs
+++ Star_Pyramid_Pattern.cs
@@ -8,24 +8,24 @@ namespace Coding_Exercise_for_Interview
 {
     class Star_Pyramid_Pattern
     {
-        public static void Main()
-        {
-            Console.WriteLine("Enter the line to print pattern");
-            int n = Convert.ToInt32(Console.ReadLine());
-            for (int i = 1; i <= n; i++)
-            {
-                for (int j = 1; j <= (n-i); j++)
-                {
-                    Console.Write(" ");
-                }
-                for(int k=1; k<i*2;k++)
-                {
-                    Console.Write("*");
-                }
+        //public static void Main()
+        //{
+        //    Console.WriteLine("Enter the line to print pattern");
+        //    int n = Convert.ToInt32(Console.ReadLine());
+        //    for (int i = 1; i <= n; i++)
+        //    {
+        //        for (int j = 1; j <= (n-i); j++)
+        //        {
+        //            Console.Write(" ");
+        //        }
+        //        for(int k=1; k<i*2;k++)
+        //        {
+        //            Console.Write("*");
+        //        }
 
-                Console.WriteLine();
-            }
-            Console.ReadLine();
-        }
+        //        Console.WriteLine();
+        //    }
+        //    Console.ReadLine();
+        //}
     }
 }
