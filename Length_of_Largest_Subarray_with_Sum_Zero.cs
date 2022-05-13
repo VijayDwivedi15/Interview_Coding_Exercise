9db7e5ebc991809741cf8e31893daed659397022
vijaydwivedi125 <vijaydwivedi125@gmail.com>
2022-05-13 22:15:25 +0530

    Length of longest subarray with sum of 0

---
+++ Length_of_Largest_Subarray_with_Sum_Zero.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Exercise_for_Interview
+{
+    class Length_of_Largest_Subarray_with_Sum_Zero
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 1, 4, 9, 5, 1, 4 };
+
+            int max_len = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                int curr_sum = 0;
+                for (int j = i; j < arr.Length; j++)
+                {
+                    curr_sum += arr[j];
+
+                    if (curr_sum == 0)
+                        max_len = Math.Max(max_len,
+                                           j - i + 1);
+                }
+            }
+            Console.WriteLine("Length of the longest 0 sum subarray is " + max_len);
+            Console.ReadLine();
+        }
+    }
+}
--- Single_Most_Occurance_of_Chars_in_Word.cs
+++ Single_Most_Occurance_of_Chars_in_Word.cs
@@ -8,30 +8,30 @@ namespace Coding_Exercise_for_Interview
 {
     class Single_Most_Occurance_of_Chars_in_Word
     {
-        public static void Main()
-        {
-            String str = "abracadabra";
-            int[] charCount = new int[256];
-            int length = str.Length;
-            for (int i = 0; i < length; i++)
-            {
-                charCount[str[i]]++;
-            }
-            int maxCount = -1;
-            char character = ' ';
-            for (int i = 0; i < length; i++)
-            {
-                if (maxCount < charCount[str[i]])
-                {
-                    maxCount = charCount[str[i]];
-                    character = str[i];
-                }
-            }
-            Console.WriteLine("The string is: " + str);
-            Console.WriteLine("The highest occurring character is: " + character + " = " + maxCount);
+        //public static void Main()
+        //{
+        //    String str = "abracadabra";
+        //    int[] charCount = new int[256];
+        //    int length = str.Length;
+        //    for (int i = 0; i < length; i++)
+        //    {
+        //        charCount[str[i]]++;
+        //    }
+        //    int maxCount = -1;
+        //    char character = ' ';
+        //    for (int i = 0; i < length; i++)
+        //    {
+        //        if (maxCount < charCount[str[i]])
+        //        {
+        //            maxCount = charCount[str[i]];
+        //            character = str[i];
+        //        }
+        //    }
+        //    Console.WriteLine("The string is: " + str);
+        //    Console.WriteLine("The highest occurring character is: " + character + " = " + maxCount);
             
-            Console.ReadLine();
-        }
+        //    Console.ReadLine();
+        //}
         
     }
 }
