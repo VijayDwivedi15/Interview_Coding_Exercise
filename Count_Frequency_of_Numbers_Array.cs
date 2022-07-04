ad6c42ee16679b96f84c68fc1854cf3764951e36
vijaydwivedi125 <vijaydwivedi125@gmail.com>
2022-07-04 17:12:31 +0530

    Number freuency count from array

---
+++ Count_Frequency_of_Numbers_Array.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Exercise_for_Interview
+{
+    class Count_Frequency_of_Numbers_Array
+    {
+        public static void Main()
+        {
+
+            int[] arr = { 1, 3, 4, 1, 3, 5, 2 };
+
+            int[] freq = new int[arr.Length];
+
+            int visited = -1;
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int count = 1;
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                        freq[j] = visited;
+                    }
+                }
+
+                if (freq[i] != visited)
+                {
+                    freq[i] = count;
+                }
+            }
+
+            for (int i = 0; i < freq.Length; i++)
+            {
+
+                if (freq[i] != visited)
+                {
+
+                    Console.WriteLine(arr[i] + "=" + freq[i]);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+}
--- Sorting_Array_Without_Sort_Method.cs
+++ Sorting_Array_Without_Sort_Method.cs
@@ -8,29 +8,29 @@ namespace Coding_Exercise_for_Interview
 {
     class Sorting_Array_Without_Sort_Method
     {
-        public static void Main()
-        {
-            int[] arr = new int[] { 2, 9, 4, 3, 5, 1, 7 };
-            int temp = 0;
+        //public static void Main()
+        //{
+        //    int[] arr = new int[] { 2, 9, 4, 3, 5, 1, 7 };
+        //    int temp = 0;
 
-            for (int i = 0; i < arr.Length - 1; i++)
-            {
-                for (int j = i + 1; j < arr.Length; j++)
-                {
-                    if(arr[i]>arr[j])
-                    {
-                        temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
-                    }
-                }
-            }
+        //    for (int i = 0; i < arr.Length - 1; i++)
+        //    {
+        //        for (int j = i + 1; j < arr.Length; j++)
+        //        {
+        //            if(arr[i]>arr[j])
+        //            {
+        //                temp = arr[i];
+        //                arr[i] = arr[j];
+        //                arr[j] = temp;
+        //            }
+        //        }
+        //    }
 
-            foreach (var item in arr)
-            {
-                Console.WriteLine(item);
-            }
-            Console.ReadLine();
-        }
+        //    foreach (var item in arr)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+        //    Console.ReadLine();
+        //}
     }
 }
