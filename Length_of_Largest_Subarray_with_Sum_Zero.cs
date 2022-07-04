2030c3d2ef07160250172bca8fac6fb7e7148d5b
vijaydwivedi125 <vijaydwivedi125@gmail.com>
2022-07-04 15:44:34 +0530

    Array sort without sort() method

--- Length_of_Largest_Subarray_with_Sum_Zero.cs
+++ Length_of_Largest_Subarray_with_Sum_Zero.cs
@@ -8,26 +8,26 @@ namespace Coding_Exercise_for_Interview
 {
     class Length_of_Largest_Subarray_with_Sum_Zero
     {
-        public static void Main(string[] args)
-        {
-            int[] arr = { 1, 4, 9, 5, 1, 4 };
+        //public static void Main(string[] args)
+        //{
+        //    int[] arr = { 1, 4, 9, 5, 1, 4 };
 
-            int max_len = 0;
-            for (int i = 0; i < arr.Length; i++)
-            {
+        //    int max_len = 0;
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
 
-                int curr_sum = 0;
-                for (int j = i; j < arr.Length; j++)
-                {
-                    curr_sum += arr[j];
+        //        int curr_sum = 0;
+        //        for (int j = i; j < arr.Length; j++)
+        //        {
+        //            curr_sum += arr[j];
 
-                    if (curr_sum == 0)
-                        max_len = Math.Max(max_len,
-                                           j - i + 1);
-                }
-            }
-            Console.WriteLine("Length of the longest 0 sum subarray is " + max_len);
-            Console.ReadLine();
-        }
+        //            if (curr_sum == 0)
+        //                max_len = Math.Max(max_len,
+        //                                   j - i + 1);
+        //        }
+        //    }
+        //    Console.WriteLine("Length of the longest 0 sum subarray is " + max_len);
+        //    Console.ReadLine();
+        //}
     }
 }
---
+++ Sorting_Array_Without_Sort_Method.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Exercise_for_Interview
+{
+    class Sorting_Array_Without_Sort_Method
+    {
+        public static void Main()
+        {
+            int[] arr = new int[] { 2, 9, 4, 3, 5, 1, 7 };
+            int temp = 0;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if(arr[i]>arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
